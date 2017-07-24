58421a9d4dbaa2146ac6171000cc16d7b9b0a39f
Hemant <hemantmali21@gmail.com>
2017-07-24 09:57:35 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TemplateDrivenComponent } from './template-driven/template-driven.component';
-import {OkbuttonComponent} from './okbutton/okbutton.component';
+import {SaveButtonComponent} from './save/save-button.component';
 import { ModelDrivenComponent } from './model-driven/model-driven.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateDrivenComponent,
-    OkbuttonComponent,
+    SaveButtonComponent,
     ModelDrivenComponent
   ],
   imports: [
